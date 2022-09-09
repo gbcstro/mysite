a78043f811db7592cf77859657228641159a83c7
Gabriel Castro <gabminer05@gmail.com>
2022-09-09 19:49:31 +0800

    update vercel database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cargo-loading',
+        'USER': 'postgres',
+        'PASSWORD': 'gabriel2001',
+        'HOST': 'localhost',
+        'POST': '5432'
     }
 }
 
