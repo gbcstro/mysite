25838176d68e6bb76a7e759420eed7020452149f
Gabriel Castro <gabminer05@gmail.com>
2022-09-09 10:11:36 +0800

    deploy fix #1

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'cargoloading.apps.CargoloadingConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -135,9 +134,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'hyy9nqrlt',
-    'API_KEY': '486916525494764',
-    'API_SECRET': 'd738gFTRLaKiW1B3QwH_E0cqjm0',
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
